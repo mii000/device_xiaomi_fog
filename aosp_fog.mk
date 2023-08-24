e036b66759985904a9d98740b4afb22c47ebce8c
Tanmsyffa <sltnmsyffaa@gmail.com>
2023-08-24 07:10:24 +0700

    fog: Build aperature camera from source

--- aosp_fog.mk
+++ aosp_fog.mk
@@ -11,6 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Elixir stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_BUILD_APERTURE_CAMERA := true
 ELIXIR_BUILD_TYPE := OFFICIAL
 
 # Inherit from fog device
